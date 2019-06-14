bc891ddce0597afebcfd77668f507548477daebb
Mark Wirblich <mark@wirblich.com>
2019-06-14 11:19:08 +0200

    reintroduce getDerivedStateFromProps

--- Swiper.js
+++ Swiper.js
@@ -36,6 +36,14 @@ const rebuildStackAnimatedValues = (props) => {
 }
 
 class Swiper extends Component {
+  static getDerivedStateFromProps (props, state) {
+    return {
+      ...state,
+      ...calculateCardIndexes(props.cardIndex, props.cards),
+      cards: props.cards,
+    }
+  }
+
   constructor (props) {
     super(props)
 
